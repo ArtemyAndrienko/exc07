a442e63a7e94f03b827f85d80c5a8d95debf8440
ArtemyAndrienko <artemy.Andrienko@gmail.com>
2021-09-05 11:34:18 +0000

    Results GO 05-07. fix bugs, add remarks

--- bench_test.go
+++ bench_test.go
@@ -7,7 +7,7 @@ import (
 func BenchmarkWithoutPool(b *testing.B) {
 	var cc *Counter
 	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
+	for i := 0; i < 10; i++ {
 		for j := 0; j < 10000; j++ {
 			cc = &Counter{A: 0, B: 1}
 			b.StopTimer()
@@ -20,12 +20,11 @@ func BenchmarkWithoutPool(b *testing.B) {
 func BenchmarkWithPool(b *testing.B) {
 	var cc *Counter
 	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
+	for i := 0; i < 10; i++ {
 		for j := 0; j < 10000; j++ {
 			cc = counterPool.Get().(*Counter)
 			b.StopTimer()
-			cc.A++
-			cc.B++
+			cc.increment()
 			b.StartTimer()
 			counterPool.Put(cc)
 		}
